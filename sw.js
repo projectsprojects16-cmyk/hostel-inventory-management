56393931978496a599330247b7abfe7a9162411f
projectsprojects16-cmyk <projectsprojects16@gmail.com>
2026-01-04 03:25:51 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,2 @@
-self.addEventListener("install", e => {
-  self.skipWaiting();
-});
-
-self.addEventListener("fetch", () => {});
+self.addEventListener("install",e=>self.skipWaiting());
+self.addEventListener("fetch",()=>{});
